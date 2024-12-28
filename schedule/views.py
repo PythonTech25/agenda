c53a8e3d3375c989a636b38b66de3fc4f47251f2
Paulo Xavier Vieira <pxvwestrock@gmail.com>
2024-12-28 00:46:58 +0000

    Atualizando o sistema

--- schedule/views.py
+++ schedule/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Contact
 
 
 # Create your views here.
 def index_login(request):
-    return render(request, 'pages/login_logout/login.html')
+    contacts = Contact.objects.all()
+
+    context = {
+        'contacts': contacts,
+    }
+    return render(request, 'pages/login_logout/login.html', context=context)
