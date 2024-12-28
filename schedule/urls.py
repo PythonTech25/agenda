3f9e73fdf19819e8cff16bcfb9315e468aa60bf5
Paulo Xavier Vieira <pxvwestrock@gmail.com>
2024-12-28 07:08:26 +0000

    att

--- schedule/urls.py
+++ schedule/urls.py
@@ -2,5 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # login / logout
     path("", views.index_login, name='index_login'),
+
+    # user crud
+    path('user/create/', views.user_create, name='user_create'),
+
+    # contact crud
+    path(
+        'contacts/create/',
+        views.contact_create,
+        name='contact_create'
+    ),
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Contact
+
 
 
 # Create your views here.
 def index_login(request):
-    contacts = Contact.objects.all()
+    login = ''
 
-    context = {
-        'contacts': contacts,
-    }
+    context = {'login': login}
     return render(request, 'pages/login_logout/login.html', context=context)
+
+
+def user_create(request):
+    users = ''
+    context = {'users': users}
+    return render(request, 'pages/users/user_create.html', context=context)
+
+
+def contact_create(request, contact_id):
+    contact = get_object_or_404(Contact.objects, pk=contact_id)
+    context = {'contacts': contact}
+    return render(request, 'pages/contacts/contacts_create.html', context=context)
